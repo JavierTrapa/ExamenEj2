90ae74534b88d8714273cf7193fe288bcbe0f178
JavierTrapa <javier.ftrapa@gmail.com>
2019-03-12 19:39:02 +0100

    commit

---
+++ Ejercicio2/src/main/java/Evaluacion.java
@@ -0,0 +1,18 @@
+
+public class Evaluacion {
+
+	public static void main(String[] args) {
+		
+		Evaluacion.toJson();
+
+	}
+	
+	public static String Alumnos(){
+		String Nombre= "Juan";
+		String Apellidos= "Martinez";
+		int Curso= 3;
+		int[] notas= {4,6,7,4,5};
+		return Nombre;
+	}
+
+}
